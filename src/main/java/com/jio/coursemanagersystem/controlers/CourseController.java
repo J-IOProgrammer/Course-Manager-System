2391cc486770fde89727213e92f8b24a9b51b652
J-IOProgrammer <fg.bonny@outlook.com>
2023-09-21 18:47:18 +0430

    create controlers

---
+++ src/main/java/com/jio/coursemanagersystem/controlers/CourseController.java
@@ -0,0 +1,10 @@
+package com.jio.coursemanagersystem.controlers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+}
---
+++ src/main/java/com/jio/coursemanagersystem/controlers/PersonController.java
@@ -0,0 +1,13 @@
+package com.jio.coursemanagersystem.controlers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+}
+
+
+
