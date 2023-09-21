f49c58c2c971270ed7d0cb35f5958092bea871c9
J-IOProgrammer <fg.bonny@outlook.com>
2023-09-21 17:26:40 +0430

    start project

---
+++ src/main/java/com/jio/coursemanagersystem/CourseManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jio.coursemanagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseManagerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jio/coursemanagersystem/CourseManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jio.coursemanagersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
