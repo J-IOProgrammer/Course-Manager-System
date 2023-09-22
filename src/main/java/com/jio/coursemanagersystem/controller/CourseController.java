61fc3fb4bc957b07f78823ffcd09b1ce6eca2462
J-IOProgrammer <fg.bonny@outlook.com>
2023-09-22 13:00:15 +0330

    add configs for database in application.properties

---
+++ src/main/java/com/jio/coursemanagersystem/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.jio.coursemanagersystem.controlers;
+
+import com.jio.coursemanagersystem.model.Course;
+import com.jio.coursemanagersystem.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping("/{courseId}")
+    public Mono<Course> getCourseById(@PathVariable Long courseId) {
+        return courseService.getCourseById(courseId);
+    }
+
+    @PostMapping
+    public Mono<Course> createCourse(@RequestBody Course course) {
+        return courseService.createCourse(course);
+    }
+}
+
---
+++ src/main/java/com/jio/coursemanagersystem/controller/PersonController.java
@@ -0,0 +1,35 @@
+package com.jio.coursemanagersystem.controlers;
+
+import com.jio.coursemanagersystem.model.Person;
+import com.jio.coursemanagersystem.service.PersonService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping("/{personId}")
+    public Mono<Person> getPersonById(@PathVariable Long personId) {
+        return personService.getPersonById(personId);
+    }
+
+    @PostMapping
+    public Mono<Person> createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+}
+
+
+
+
